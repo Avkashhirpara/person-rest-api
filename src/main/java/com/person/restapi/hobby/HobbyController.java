15a7e99cf32bc0cf305bb1221c8a281fc4ee5ccd
Avkashhirpara <avkash.av1987@gmail.com>
2020-02-13 15:12:45 +0530

    HobbyController : passed test case for create a new hobby.

--- src/main/java/com/person/restapi/hobby/HobbyController.java
+++ src/main/java/com/person/restapi/hobby/HobbyController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,11 @@ public class HobbyController {
     @GetMapping("/hobbies/{id}")
     ResponseEntity<Hobby> getHobbyByID(@PathVariable(value = "id") Long hobbyId){
         return new ResponseEntity<>(hobbyService.findById(hobbyId),HttpStatus.OK);
+    }
+
+    @PostMapping("/hobbies")
+    ResponseEntity<Hobby> createHobby(@RequestBody Hobby hobby){
+        return new ResponseEntity<>(hobbyService.save(hobby), HttpStatus.CREATED);
     }
 
 
