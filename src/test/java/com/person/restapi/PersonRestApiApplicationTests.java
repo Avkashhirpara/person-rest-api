5c54865bf93fbfc44db507be1d663d9bee8dd821
Avkashhirpara <avkash.av1987@gmail.com>
2020-02-13 12:40:46 +0530

    Adding Initial project setup.

---
+++ src/test/java/com/person/restapi/PersonRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.person.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
