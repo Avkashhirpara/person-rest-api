5416ee4ee15a4040d53e93135b1876178d7c73d6
Avkashhirpara <avkash.av1987@gmail.com>
2020-02-13 15:17:56 +0530

    HobbyController : pass test case for update an existing hobby.

--- src/main/java/com/person/restapi/hobby/HobbyController.java
+++ src/main/java/com/person/restapi/hobby/HobbyController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,13 @@ public class HobbyController {
     @PostMapping("/hobbies")
     ResponseEntity<Hobby> createHobby(@RequestBody Hobby hobby){
         return new ResponseEntity<>(hobbyService.save(hobby), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/hobbies/{id}")
+    ResponseEntity<Hobby> udpateHobby(@PathVariable(value = "id") Long hobbyId
+            , @RequestBody Hobby hobby){
+        Hobby newHobby = (hobbyService.update(hobbyId,hobby));
+        return new ResponseEntity<>(newHobby, HttpStatus.OK);
     }
 
 
