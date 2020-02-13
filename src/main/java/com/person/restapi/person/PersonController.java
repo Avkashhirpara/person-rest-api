abbe54355cdbad258b1bd970be05660b3bfed901
Avkashhirpara <avkash.av1987@gmail.com>
2020-02-13 13:50:55 +0530

    personController  : pass test case for get person by id :

--- src/main/java/com/person/restapi/person/PersonController.java
+++ src/main/java/com/person/restapi/person/PersonController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,10 +29,26 @@ public class PersonController {
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
+
     @GetMapping("/persons")
     ResponseEntity<List<Person>> getPersons(){
         return new ResponseEntity<>(personService.findAll(), HttpStatus.OK);
     }
+
+
+    @ApiOperation(value = "Retrieve person by personId ", response = Person.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved a Person "),
+            @ApiResponse(code = 401, message = "You are not authorized to view Person"),
+            @ApiResponse(code = 403, message = "Accessing Person you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The Person you were trying to reach is not found")
+    })
+    @GetMapping("/persons/{id}")
+    ResponseEntity<Person> getPersonByID(@PathVariable(value = "id") Long personId){
+        return new ResponseEntity<>(personService.findById(personId),HttpStatus.OK);
+    }
+
+
 
 
 
