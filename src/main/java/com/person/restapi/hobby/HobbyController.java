792a687126932c8b5b77b0218089dfc8eedae755
Avkashhirpara <avkash.av1987@gmail.com>
2020-02-13 15:08:34 +0530

    HobbyController : pass test case for find hobby by Id.

--- src/main/java/com/person/restapi/hobby/HobbyController.java
+++ src/main/java/com/person/restapi/hobby/HobbyController.java
@@ -3,6 +3,7 @@ package com.person.restapi.hobby;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class HobbyController {
     @GetMapping("/hobbies")
     ResponseEntity<List<Hobby>> getHobbies(){
         return new ResponseEntity<>(hobbyService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/hobbies/{id}")
+    ResponseEntity<Hobby> getHobbyByID(@PathVariable(value = "id") Long hobbyId){
+        return new ResponseEntity<>(hobbyService.findById(hobbyId),HttpStatus.OK);
     }
 
 
